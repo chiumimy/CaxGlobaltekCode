e2f409b1a8a34bb3269c4864bd5c0f4565971186
chiumimy <chiukaikai@gmail.com>
2018-05-11 16:59:21 +0800

    2018/05/11，修正CAX，新增UpLoad、TEUpLoad類別

--- 0_CaxGlobaltek/CaxGlobaltek/CaxTEUpLoad.cs
+++ 0_CaxGlobaltek/CaxGlobaltek/CaxTEUpLoad.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using NXOpen;
-using System.Windows.Forms;
-using NXOpen.Assemblies;
+using System.Text.RegularExpressions;
 
 namespace CaxGlobaltek
 {
-    public class CaxMEUpLoad : CaxUpLoad
+    public class CaxTEUpLoad : CaxUpLoad
     {
-
         /// <summary>
-        /// 拆解OIS上傳的全路徑，取得客戶、料號、客戶版次、製程版次、製程序
+        /// 拆解CAM上傳的全路徑，取得客戶、料號、客戶版次、製程版次、製程序
         /// </summary>
         /// <param name="partFullPath"></param>
         /// <param name="sPartInfo"></param>
@@ -23,26 +20,7 @@ namespace CaxGlobaltek
             try
             {
                 SplitRoot(partFullPath);
-                OpNum = Path.GetFileNameWithoutExtension(partFullPath).Split(new string[] { "OIS" }, StringSplitOptions.RemoveEmptyEntries)[1];
-            }
-            catch (System.Exception ex)
-            {
-                return false;
-            }
-            return true;
-        }
-        /// <summary>
-        /// 拆解ME模檢治具上傳的全路徑，取得客戶、料號、客戶版次、製程版次、製程序
-        /// </summary>
-        /// <param name="partFullPath"></param>
-        /// <param name="sPartInfo"></param>
-        /// <returns></returns>
-        public bool SplitMEFixInsPartFullPath(string partFullPath)
-        {
-            try
-            {
-                SplitRoot(partFullPath);
-                OpNum = Path.GetFileNameWithoutExtension(partFullPath).Split(new string[] { "_OIS" }, StringSplitOptions.RemoveEmptyEntries)[1];
+                OpNum = Path.GetFileNameWithoutExtension(partFullPath).Split(new string[] { "_OP" }, StringSplitOptions.RemoveEmptyEntries)[1];
                 OpNum = OpNum.Substring(0, 3);
             }
             catch (System.Exception ex)
@@ -51,6 +29,26 @@ namespace CaxGlobaltek
             }
             return true;
         }
-        
+        /// <summary>
+        /// 拆解TE模檢治具上傳的全路徑，取得客戶、料號、客戶版次、製程版次、製程序
+        /// </summary>
+        /// <param name="partFullPath"></param>
+        /// <param name="sPartInfo"></param>
+        /// <returns></returns>
+        public bool SplitTEFixInsPartFullPath(string partFullPath)
+        {
+            try
+            {
+                SplitRoot(partFullPath);
+                OpNum = Path.GetFileNameWithoutExtension(partFullPath).Split(new string[] { "_OP" }, StringSplitOptions.RemoveEmptyEntries)[1];
+                OpNum = OpNum.Substring(0, 3);
+            }
+            catch (System.Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
+
